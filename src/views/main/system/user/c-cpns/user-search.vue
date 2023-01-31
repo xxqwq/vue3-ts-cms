2f88be8e35fc6060aa05c32bfb5c5240af1c94bb
xxqwq <2524447983@qq.com>
2023-01-31 10:55:53 +0800

    '重置按钮功能实现'

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,31 +1,45 @@
 <template>
   <div class="search">
     <!-- 输入搜索关键字的表单 -->
-    <el-form label-width="80px" size="large">
+    <el-form label-width="80px" size="large" :model="searchForm" ref="formRef">
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-form-item label="用户名">
-            <el-input placeholder="请输入查询的用户名" /> </el-form-item
+          <el-form-item label="用户名" prop="name">
+            <el-input
+              placeholder="请输入查询的用户名"
+              v-model="searchForm.name"
+            /> </el-form-item
         ></el-col>
         <el-col :span="8">
-          <el-form-item label="真实姓名">
-            <el-input placeholder="请输入查询的真实姓名" /> </el-form-item
+          <el-form-item label="真实姓名" prop="realname">
+            <el-input
+              placeholder="请输入查询的真实姓名"
+              v-model="searchForm.realname"
+            /> </el-form-item
         ></el-col>
         <el-col :span="8">
-          <el-form-item label="手机号码">
-            <el-input placeholder="请输入查询的手机号码" /> </el-form-item
+          <el-form-item label="手机号码" prop="cellphone">
+            <el-input
+              placeholder="请输入查询的手机号码"
+              v-model="searchForm.cellphone"
+            /> </el-form-item
         ></el-col>
 
         <el-col :span="8">
-          <el-form-item label="状态">
-            <el-select placeholder="请选择查询的状态" style="width: 100%">
-              <el-option label="启用" value="1" />
-              <el-option label="禁用" value="2" />
+          <el-form-item label="状态" prop="enable">
+            <el-select
+              placeholder="请选择查询的状态"
+              style="width: 100%"
+              v-model="searchForm.enable"
+            >
+              <el-option label="启用" :value="1" />
+              <el-option label="禁用" :value="2" />
             </el-select> </el-form-item
         ></el-col>
         <el-col :span="8">
-          <el-form-item label="日期">
+          <el-form-item label="日期" prop="createAt">
             <el-date-picker
+              v-model="searchForm.createAt"
               type="daterange"
               range-separator="-"
               start-placeholder="开始时间"
@@ -36,13 +50,31 @@
     </el-form>
     <!-- 重置和搜索的表单 -->
     <div class="btns">
-      <el-button icon="Refresh">重置</el-button>
-      <el-button icon="Search" type="primary">搜索</el-button>
+      <el-button icon="Refresh" @click="handleResetClick">重置</el-button>
+      <el-button icon="Search" type="primary" @click="handleQueryClick"
+        >搜索</el-button
+      >
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { ElForm } from 'element-plus'
+import { reactive, ref } from 'vue'
+const searchForm = reactive({
+  name: '',
+  realname: '',
+  cellphone: '',
+  enable: 1,
+  createAt: []
+})
+//重置操作
+const formRef = ref<InstanceType<typeof ElForm>>()
+function handleResetClick() {
+  formRef.value?.resetFields()
+}
+//搜索操作
+function handleQueryClick() {}
 </script>
 
 <style lang="less" scoped>
