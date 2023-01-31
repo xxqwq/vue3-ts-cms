2ea94403bd0f10622f464e06428e21b226807b7e
xxqwq <2524447983@qq.com>
2023-01-31 11:02:11 +0800

    'header的布局'

---
+++ src/views/main/system/user/c-cpns/user-content.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <div class="header">
+      <h3 class="title">用户列表</h3>
+      <el-button type="primary">新建用户</el-button>
+    </div>
+    <div class="table">不过</div>
+    <div class="pagination"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="less" scoped>
+.content {
+  padding: 15px;
+  background-color: #fff;
+  margin-top: 20px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 10px;
+}
+</style>
+
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="user">
     <user-search />
-    <div class="content">
-      <div class="header"></div>
-      <div class="table"></div>
-      <div class="pagination"></div>
-    </div>
+    <user-content />
   </div>
 </template>
 
 <script setup lang="ts">
 import UserSearch from './c-cpns/user-search.vue'
+import UserContent from './c-cpns/user-content.vue'
 </script>
 
 <style lang="less" scoped>
 .user {
-  color: red;
   border-radius: 8px;
   overflow: hidden;
 }
